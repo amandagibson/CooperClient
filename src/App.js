0a9f0a715725e91e60c88fb624ac599c7ef67db1
vick3d <viktoro0090@gmail.com>
2019-03-05 18:06:23 +0100

    extracts Input fields from app to new component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DisplayCooperResult from './Components/DisplayCooperResult';
+import InputFields from './Components/InputFields'
 
 
 class App extends Component {
@@ -21,29 +22,17 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<div>
-					<label>Distance</label>
-					<input id="distance" onChange={this.onChange.bind(this)}></input>
-				</div>
+				<InputFields
+					inputChangeHandler={this.onChange.bind(this)}
+				/>
 
-				<select id="gender" onChange={this.onChange.bind(this)}>
-          <option value="female">Female</option>
-          <option value="male">Male</option>
-        </select>
-
-        <div>
-          <label>Age</label>
-          <input id="age" onChange={this.onChange.bind(this)}></input>
-        </div>
-
-        <DisplayCooperResult
+				<DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
           age={this.state.age}
         />
-
 			</div>
-			);
+			)
 		}
 	}
 
