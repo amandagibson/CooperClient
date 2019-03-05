cac774037d25a7e185365a4aaaa6ef05b02ee593
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-03-05 20:00:55 +0100

    adds calculator, updates app component, adds gender, distance and age

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
+import DisplayCooperResult from './Components/DisplayCooperResult';
+
 
 class App extends Component {
-  render() {
-    return (
-     <div>
+	constructor(props) {
+		super(props);
+		this.state = {
+			distance: '',
+			gender: 'female',
+			age: ''
+		}
+	}
+
+	onChange(event) {
+		this.setState({
+			[event.target.id]: event.target.value
+		})
+	}
+
+	render() {
+		return (
+			<div>
 				<div>
 					<label>Distance</label>
-					<input id="distance"></input>
+					<input id="distance" onChange={this.onChange.bind(this)}></input>
 				</div>
 
-				<select id="gender">
-					<option value="female">Female</option>
-					<option value="male">Male</option>
-				</select>
+				<select id="gender" onChange={this.onChange.bind(this)}>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
 
-				<div>
-					<label>Age</label>
-					<input id="age"></input>
-				</div>
-		 </div>
-    );
-  }
-}
+        <div>
+          <label>Age</label>
+          <input id="age" onChange={this.onChange.bind(this)}></input>
+        </div>
 
-export default App;
+        <DisplayCooperResult
+          distance={this.state.distance}
+          gender={this.state.gender}
+          age={this.state.age}
+        />
+
+			</div>
+			);
+		}
+	}
+
+	export default App;
