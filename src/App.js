1e099c4679f118d929889b8f90a5dd57e526fab9
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-03-06 13:42:35 +0100

    adds user can login feature, user suthentication, log in functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import DisplayCooperResult from './Components/DisplayCooperResult';
 import InputFields from './Components/InputFields'
+import LoginForm from './Components/LoginForm';
+import { authenticate } from './Modules/Auth';
 
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			distance: '',
-			gender: 'female',
-			age: ''
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      distance: '',
+      gender: 'female',
+      age: '',
+      renderLoginForm: false,
+      authenticated: false,
+      email: '',
+      password: '',
+      message: ''
+    }
+  }
 
 	onChange(event) {
 		this.setState({
@@ -19,21 +26,61 @@ class App extends Component {
 		})
 	}
 
-	render() {
-		return (
-			<div>
-				<InputFields
-					inputChangeHandler={this.onChange.bind(this)}
-				/>
+	async onLogin(e) {
+		e.preventDefault();
+		let resp = await authenticate(this.state.email, this.state.password)
+		if (resp.authenticated === true) {
+			this.setState({ authenticated: true });
+		} else {
+			this.setState({ message: resp.message, renderLoginForm: false })
+		}
+	}
 
-				<DisplayCooperResult
+	render() {
+    let renderLogin;
+    let user;
+
+    if (this.state.authenticated === true) {
+      user = JSON.parse(sessionStorage.getItem('credentials')).uid;
+      renderLogin = (
+        <p>Hi {user}</p>
+      )
+    } else {
+      if (this.state.renderLoginForm === true) {
+        renderLogin = (
+          <>
+            <LoginForm
+              loginHandler={this.onLogin.bind(this)}
+              inputChangeHandler={this.onChange.bind(this)}
+            />
+          </>
+        )
+      } else {
+        renderLogin = (
+          <>
+            <button id="login" onClick={() => this.setState({ renderLoginForm: true })}>Login</button>
+            <p>{this.state.message}</p>
+          </>
+        )
+      }
+    }
+
+    return (
+      <div>
+        <InputFields
+          inputChangeHandler={this.onChange.bind(this)}
+        />
+
+        <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
           age={this.state.age}
         />
-			</div>
-			)
-		}
-	}
+        {renderLogin}
 
-	export default App;
+      </div>
+    );
+  }
+}
+
+export default App;
