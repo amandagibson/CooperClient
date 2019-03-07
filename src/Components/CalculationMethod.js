e5acc243a2fa45a842a83d14a09de987ea90c384
vick3d <viktoro0090@gmail.com>
2019-03-07 10:30:44 +0100

    adds bmi calculator

---
+++ src/Components/CalculationMethod.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class CalculationMethod extends Component {
+	render () {
+
+		return(
+			<div>
+			<select id="method" name="method" onChange= { this.props.onChangeValue}>
+			<option value="metric">metric</option>
+			<option value="imperial">imperial</option>
+			</select>
+			</div>
+			)
+		}
+	}
+
+	export default CalculationMethod
