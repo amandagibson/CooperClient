713ab6ce104385e066f274af680afa67071895bb
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-03-09 14:48:35 +0100

    hides password

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -2,36 +2,39 @@ import React from 'react';
 import { Form, Button, Header } from 'semantic-ui-react';
 
 const LoginForm = (props) => {
-  return (
+	return (
 		<>
-			<Form type="large">
-				<Header as="h1"
-				>Login
-				</Header>
-				<Form.Input
-					fluid
-					id="email"
-					placeholder="Email"
-					onChange={props.inputChangeHandler}
-				/>
+		<Form type="large">
+		<Header as="h1"
+		>Login
+		</Header>
+		<Form.Input
+		fluid
+		id="email"
+		placeholder="Email"
+		onChange={props.inputChangeHandler}
+		/>
 
-				<Form.Input
-					fluid
-					id="password"
-					placeholder="Password"
-					onChange={props.inputChangeHandler}
-				/>
+		<Form.Input
+		fluid
+		icon='lock'
+		iconPosition='left'
+		placeholder='Password'
+		type='password'
+		id="password"
+		onChange={props.inputChangeHandler}
+		/>
 
-				<Button
-					id="submit"
-					color="green"
-					onClick={(e) => props.loginHandler(e)}
-				>
-					Submit
-				</Button>
-			</Form>
+		<Button
+		id="submit"
+		color="green"
+		onClick={(e) => props.loginHandler(e)}
+		>
+		Submit
+		</Button>
+		</Form>
 		</>
-  )
-}
+		)
+	}
 
-export default LoginForm;
+	export default LoginForm;
