c353cb03fb90746b65b05c9456f3de3eedf7cc60
vick3d <viktoro0090@gmail.com>
2019-03-09 23:47:25 +0100

    fix calls to API for getting data to cooperGraph

--- src/Components/DisplayCooperGraph.js
+++ src/Components/DisplayCooperGraph.js
@@ -17,27 +17,30 @@ class DisplayCooperGraph extends Component {
 	async getPerformanceData() {
 		let result = await getData();
 		this.setState({performanceData: result.data.entries}, () => {
-			this.props.handleCooperGraph();
+			this.props.graphUpdated();
 		})
 	}
 
 	render () {
 		let dataIndex;
 
-		if (this.props.renderCooperGraph === true) {
+		if (this.props.updateCooperGraph === true) {
 			this.getPerformanceData();
 		}
 		if (this.state.performanceData != null) {
+
 			const distances = []
 			const labels = []
 			this.state.performanceData.forEach(entry => {
-      distances.push(entry.data.distance)
+			distances.push(entry.data.distance)
+			labels.push(entry.data.message)
 			})
+			debugger;
 			const data = {
 				datasets: [{
 					data: distances,
-					label: 'Stored runs',
 				}],
+				labels: labels,
 			}
 			dataIndex = (
 				<>
